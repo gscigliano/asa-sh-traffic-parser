fff9950bfb86f3b4ab7bb494b8df7b43058c285d
Gabriele Scigliano <31177235+gscigliano@users.noreply.github.com>
2017-08-23 19:20:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,28 @@
+# paste the sh traffic in some file and pass the file as argument to this script
+# produces a report on the 1 minute rate of pps/mpbs
 
-#!/bin/bash
-# paste the sh traffic | i ^[a-zA-Z]|1 minute input rate
-# in shtraffic.txt
-# excluding internal interfaces
+if [ $# -eq 0 ]; then printf "\n\n missing shtraffic filename.\nUSAGE: script filename\n\n"; exit;fi
 
-PPS=$(cat shtraffic.txt | awk '{print $5}' | egrep [0-9] | tr '\n' '+' | sed 's/\+$//g')
-MBPS=$(cat  shtraffic.txt | awk '{print $7}' | egrep [0-9] | tr '\n' '+' | sed 's/\+$//g;s/^/scale=6\;(8\/1000000)*(/g;s/$/)/g')
 
-printf "\n$(echo $PPS | bc) PPS\n\n$(echo $MBPS | bc) Mbps\n\n"
+PPS=$(cat  $1 | tr '\n' '|' | sed 's/----------------------------------------/\n/' \
+ | grep -v Aggr | tr '|' '\n' | egrep "^[^ ].+:$|1 minute input rate" \
+ | awk '{print $5}' | egrep [0-9] | tr '\n' '+' | sed 's/\+$//g')
+
+MBPS=$(cat  $1 | tr '\n' '|' | sed 's/----------------------------------------/\n/' \
+ | grep -v Aggr | tr '|' '\n' | egrep "^[^ ].+:$|1 minute input rate" \
+ | awk '{print $7}' | egrep [0-9] | tr '\n' '+' | sed 's/\+$//g;s/^/scale=6\;(8\/1000000)*(/g;s/$/)/g')
+
+
+# nameifs stats
+printf "\n\n*** NAMEIF stats\n"
+cat  $1 | tr '\n' '|' | sed 's/----------------------------------------/\n/' \
+ | grep -v Aggr | tr '|' '\n' | egrep "^[^ ].+:$|1 minute input rate"
+
+# physical IFs stats
+printf "\n\n*** Physical IFs stats\n"
+cat  $1 | tr '\n' '|' | sed 's/----------------------------------------/\n/' \
+ | grep  Aggr | tr '|' '\n' | egrep "^[^ ].+:$|1 minute input rate"
+
+# summary
+
+printf "\n\n*** SUMMARY:\n$(echo $PPS | bc) PPS\n\n$(echo $MBPS | bc) Mbps\n\n"
